96ae4eb88918f7b6afd8185cae2e45b6c3d2bc8b
ayshiff <remi.d45@gmail.com>
2019-01-17 15:19:58 +0100

    fetaure: add seed templates

---
+++ server/database/seeds/SongSeeder.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/*
+|--------------------------------------------------------------------------
+| SongSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use("Factory");
+
+class SongSeeder {
+  async run() {}
+}
+
+module.exports = SongSeeder;
