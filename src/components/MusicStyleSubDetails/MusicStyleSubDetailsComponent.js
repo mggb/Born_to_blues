e898b217fc801472279a0f2ba23820c20a9da85b
ayshiff <remi.d45@gmail.com>
2019-01-28 22:01:50 +0100

    improve: dynamic data

--- src/components/MusicStyleSubDetails/MusicStyleSubDetailsComponent.js
+++ src/components/MusicStyleSubDetails/MusicStyleSubDetailsComponent.js
@@ -1,33 +1,12 @@
 //
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
+import { propEq, find, has } from "ramda";
 import pointFreeUpperCase from "../../utils/pointFreeUpperCase";
 import "./styles/MusicStyleSubDetailsComponent.css";
-import { propEq, find } from "ramda";
-
-// import musician logo
-import hendrix from "../../assets/img/vinyle-jimmi-hendrix.png";
-import presley from "../../assets/img/vinyle-elvis-presley.png";
-import stones from "../../assets/img/vinyle-rolling-stones.png";
 
 // import header Component
 import HeaderComponent from "../../utils/headerComponent";
-
-/** Fake data */
-const MUSICIANS: Array<any> = [
-  {
-    logo: hendrix,
-    name: "hendrix"
-  },
-  {
-    logo: presley,
-    name: "presley"
-  },
-  {
-    logo: stones,
-    name: "stones"
-  }
-];
 
 type Props = {
   params: {
@@ -46,12 +25,10 @@ type State = {
 
 const SUB_DETAILS = ["instruments", "electric-guitar"];
 
-export default class MusicStyleSubDetailsComponent extends Component<
-  Props,
-  State
-> {
+class MusicStyleSubDetailsComponent extends Component<Props, State> {
   state = {
-    musicStyleState: null
+    musicStyleState: null,
+    navBarState: []
   };
 
   filterNavSubDetails = (element: any) => {
@@ -63,10 +40,19 @@ export default class MusicStyleSubDetailsComponent extends Component<
    * @param {Array} musicians - The array of musicians
    * @returns {Array<any>}
    */
-  renderArtistsLinks = (musicians: Array<any>): Array<any> =>
+  renderArtistsLinks = (
+    musicians: Array<any>,
+    musicStyle,
+    musicDetail
+  ): Array<any> =>
     musicians.map(musician => (
       <div key={musician.name}>
-        <Link to={`/${musician.name}`}>
+        <Link
+          onClick={() => {
+            this.fetchData(musicStyle, musicDetail, musician.name);
+          }}
+          to={`/${musicStyle}/${musicDetail}/${musician.name}`}
+        >
           <img src={musician.logo} alt={`${musician.name} musician logo`} />
         </Link>
       </div>
@@ -91,7 +77,12 @@ export default class MusicStyleSubDetailsComponent extends Component<
     const {
       params: { musicStyle, musicStyleDetail, musicStyleSubDetail }
     } = this.props;
-    console.log(this.props);
+    this.fetchData(musicStyle, musicStyleDetail, musicStyleSubDetail);
+  };
+
+  fetchData = (musicStyle, musicStyleDetail, musicStyleSubDetail) => {
+    const hasSong = has("songs");
+
     fetch(
       `http://127.0.0.1:3333/api/${
         musicStyleDetail === "links" ? "influence" : musicStyleDetail
@@ -100,10 +91,18 @@ export default class MusicStyleSubDetailsComponent extends Component<
       .then(res => res.json())
       .then(musicStyleState => {
         this.setState({
+          navBarState: musicStyleState,
           musicStyleState: find(propEq("name", musicStyleSubDetail))(
             musicStyleState
           )
         });
+        if (musicStyleState.map(e => hasSong(e))) {
+          fetch(`http://127.0.0.1:3333/api/song/${musicStyle}`)
+            .then(res => res.json())
+            .then(songs => {
+              this.setState({ songs });
+            });
+        }
       });
   };
 
@@ -126,7 +125,8 @@ export default class MusicStyleSubDetailsComponent extends Component<
           background: ${styleColor};
       }
     `;
-    const { musicStyleState } = this.state;
+
+    const { musicStyleState, songs, navBarState } = this.state;
 
     return (
       <section>
@@ -150,26 +150,28 @@ export default class MusicStyleSubDetailsComponent extends Component<
                 <p className="text">
                   {musicStyleState && musicStyleState.description}
                 </p>
-                <div>
-                  <div className="playMusic">
-                    <a href="/">
-                      <i className="fas fa-play" />
-                    </a>
-                    <div>
-                      <p>Drift&apos;n Blues</p>
-                      <p>Eric Clapton</p>
+                {songs && (
+                  <div>
+                    <div className="playMusic">
+                      <a href="/">
+                        <i className="fas fa-play" />
+                      </a>
+                      <div>
+                        <p>{songs[0].name}</p>
+                        <p>{songs[0].author}</p>
+                      </div>
+                    </div>
+                    <div className="playMusic">
+                      <a href="/">
+                        <i className="fas fa-play" />
+                      </a>
+                      <div>
+                        <p>{songs[1].name}</p>
+                        <p>{songs[1].author}</p>
+                      </div>
                     </div>
                   </div>
-                  <div className="playMusic">
-                    <a href="/">
-                      <i className="fas fa-play" />
-                    </a>
-                    <div>
-                      <p>Drift&apos;n Blues</p>
-                      <p>Eric Clapton</p>
-                    </div>
-                  </div>
-                </div>
+                )}
                 <ul className="navSubDetails">
                   {this.renderNavigationSubDetails(
                     SUB_DETAILS,
@@ -178,7 +180,13 @@ export default class MusicStyleSubDetailsComponent extends Component<
                   )}
                 </ul>
               </div>
-              <div className="nav">{this.renderArtistsLinks(MUSICIANS)}</div>
+              <div className="nav">
+                {this.renderArtistsLinks(
+                  navBarState,
+                  params.musicStyle,
+                  params.musicStyleDetail
+                )}
+              </div>
             </section>
           </div>
         </div>
@@ -186,3 +194,5 @@ export default class MusicStyleSubDetailsComponent extends Component<
     );
   }
 }
+
+export default withRouter(MusicStyleSubDetailsComponent);
