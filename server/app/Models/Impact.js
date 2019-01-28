fed5b23d4b70d13cfdba9cfe1257442b9e687cd5
ayshiff <remi.d45@gmail.com>
2019-01-28 22:03:04 +0100

    improve(server): fix / update routes

---
+++ server/app/Models/Impact.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use("Model");
+
+class Impact extends Model {
+  // song() {
+  //   return this.hasMany("App/Models/Song");
+  // }
+  // musicStyle() {
+  //   return this.belongsTo("App/Models/MusicStyle");
+  // }
+}
+
+module.exports = Impact;
