9f30ef06ad13968bb2f5593c5e019e5acc59e34d
ayshiff <remi.d45@gmail.com>
2019-02-04 16:03:28 +0100

    improve: lint small updates

--- src/components/MusicStyleDetails/components/ArtistsComponent.js
+++ src/components/MusicStyleDetails/components/ArtistsComponent.js
@@ -2,56 +2,34 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Vinyle from "../../../utils/vinyle";
 
-// import musician logo
-import hendrix from "../../../assets/img/test.jpg";
-import presley from "../../../assets/img/vinyle-elvis-presley.png";
-import stones from "../../../assets/img/vinyle-rolling-stones.png";
-
-/** Fake data */
-const MUSICIANS: Array<any> = [
-  {
-    logo: hendrix,
-    name: "hendrix"
-  },
-  {
-    logo: presley,
-    name: "presley"
-  },
-  {
-    logo: presley,
-    name: "berry"
-  },
-  {
-    logo: stones,
-    name: "stones"
-  }
-];
-
 type Props = {
   musicStyle: string,
   musicStyleDetail: string,
   musicStyleState: any
 };
 
-const ArtistsComponent = (props: Props) => (
-  <section id="artist">
-    <div>
-      {MUSICIANS.map(musician => (
-        <div key={musician.name}>
-          <Link
-            to={`/${props.musicStyle}/${props.musicStyleDetail}/${
-              musician.name
-            }`}
-          >
-            <Vinyle
-              img={musician.logo}
-              alt={`${musician.name} musician logo`}
-            />
-          </Link>
-        </div>
-      ))}
-    </div>
-  </section>
-);
+const ArtistsComponent = (props: Props) => {
+  const { musicStyleState } = props;
+  return (
+    <section id="artist">
+      <div>
+        {musicStyleState.map(musician => (
+          <div key={musician.name}>
+            <Link
+              to={`/${props.musicStyle}/${props.musicStyleDetail}/${
+                musician.name
+              }`}
+            >
+              <Vinyle
+                img={musician.logo}
+                alt={`${musician.name} musician logo`}
+              />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default ArtistsComponent;
