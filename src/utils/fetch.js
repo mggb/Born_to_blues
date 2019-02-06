1691adb105609bfd116f1a84d79e8e3464fdedad
ayshiff <remi.d45@gmail.com>
2019-02-06 15:35:18 +0100

    fix: due to server change

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -2,7 +2,7 @@ const fetchColor = (musicStyle: string, ctx: any) => {
   fetch(`http://127.0.0.1:3333/api/music-style/${musicStyle}`)
     .then(res => res.json())
     .then(musicStyleState => {
-      ctx.setState({ color: musicStyleState.color });
+      ctx.setState({ color: musicStyleState[0].color });
     });
 };
 
